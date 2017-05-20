95cc3fb4ae8a128144b09301b74f06083b00a792
Suyeol Jeon <devxoul@gmail.com>
2017-05-20 23:42:30 +0900

    Remove RxExpect dependency

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
   name: "ReactorKit",
   dependencies: [
     .Package(url: "https://github.com/devxoul/RxSwift.git", majorVersion: 3),
-    .Package(url: "https://github.com/devxoul/RxExpect.git", majorVersion: 0),
+    //.Package(url: "https://github.com/devxoul/RxExpect.git", majorVersion: 0),
   ]
 )
